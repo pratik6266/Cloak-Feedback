0462c22f02c4965c8f7f96121a257fbf407a0e1a
Pratik Raj <pratikraj220011@gmail.com>
2025-05-12 18:48:49 +0530

    setup how to send email

---
+++ src/helpers/sendEmailVerification.ts
@@ -0,0 +1,24 @@
+import { resend } from "@/config/resend";
+import VerificationEmail from "../../emails/verification";
+import { ApiResponse } from "@/types/apiResponse";
+
+export async function sendVerificationEmail(
+  email: string,
+  userName: string,
+  verifycode: string,
+): Promise<ApiResponse>
+{
+  try {
+    await resend.emails.send({
+      from: 'onboarding@resend.dev>',
+      to: email,
+      subject: 'Verification Code',
+      react: VerificationEmail({userName, otp: verifycode})
+    });
+
+    return {success: true, message: "verification email send successfully"};
+  } catch (error) {
+    console.error("Error sending verification email", error);
+    return {success: false, message: "Failed to send verification email"};
+  }
+}
