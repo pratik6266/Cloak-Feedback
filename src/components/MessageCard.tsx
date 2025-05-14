b5a30a2c7318987633c6fad4b85283b76aa84172
Pratik Raj <pratikraj220011@gmail.com>
2025-05-15 01:04:30 +0530

    updated the request type

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -34,7 +34,9 @@ type MessageCardProps = {
 const MessageCard = ({ message, onMessageDelete }: MessageCardProps) => {
 
   const handleDeleteConfirm = async () => {
-    const response = await axios.delete<ApiResponse>(`/api/delete-message/${message.id}`);
+    const response = await axios.delete<ApiResponse>(`/api/delete-message`, {
+      params: { id: message.id }
+    });
     toast(response.data.message);
     onMessageDelete(message.id);
   }
