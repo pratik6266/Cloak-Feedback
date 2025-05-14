7bc3d0226c17fb777aaec3e808a197886e704cf3
Pratik Raj <pratikraj220011@gmail.com>
2025-05-14 17:10:00 +0530

    implement POST API endpoint to send messages with user validation

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,47 @@
+import prisma from "@/config/db.config";
+
+export async function POST(req: Request){
+  const {userName, content}: {userName: string, content: string} = await req.json();
+
+  try {
+    const user = await prisma.user.findFirst({
+      where:{
+        userName,
+      }
+    })
+
+    if(!user){
+      return Response.json({
+        success: false,
+        message: "User not found"
+      }, { status: 400})
+    }
+
+    if(!user.isAcceptingMessage){
+      return Response.json({
+        success: false,
+        message: "User is not accepting message"
+      }, { status: 403 })
+    }
+
+    const newMsg = await prisma.message.create({
+      data: {
+        content,
+        authorId: user.id
+      }
+    })
+
+    return Response.json({
+      success: true,
+      message: "Message sent successfully",
+      newMsg: newMsg
+    }, { status: 200 })
+  } 
+  catch (error) {
+    console.log("Error while sending message", error);
+    return Response.json({
+      success: false,
+      message: "Error while seindng message"
+    }, {status: 500})
+  }
+}
