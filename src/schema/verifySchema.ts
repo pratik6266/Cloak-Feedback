0d9a7f350dac56e6f6491b92f30e2cf842077ae4
Pratik Raj <pratikraj220011@gmail.com>
2025-05-14 20:59:59 +0530

    updated with regex check and only 6 digit check

--- src/schema/verifySchema.ts
+++ src/schema/verifySchema.ts
@@ -2,4 +2,6 @@ import { z } from 'zod';
 
 export const verifySchema = z.object({
   code: z.string().min(6, {message: "Verification code must be 6 digits"})
+  .max(6, {message: "Verification code must be 6 digits"})
+  .regex(/^\d+$/, {message: "OTP only contain digits"})
 })
