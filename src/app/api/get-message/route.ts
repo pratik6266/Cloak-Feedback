95065349a81b082208fe1652f21c7079c0972773
Pratik Raj <pratikraj220011@gmail.com>
2025-05-14 16:54:28 +0530

    implement get message API endpoint to retrieve user messages

---
+++ src/app/api/get-message/route.ts
@@ -0,0 +1,49 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/option";
+import prisma from "@/config/db.config";
+import { User } from "next-auth";
+
+export async function get (){
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+
+  if(!session || !user){
+    return Response.json({
+      success: false,
+      message: "User not found"
+    }, {status: 404})
+  }
+
+  const userId = user.id;
+  try {
+    const userWithMessages = await prisma.user.findUnique({
+      where: { id: userId },
+      include: {
+        messages: {
+          orderBy: {
+            createdAt: 'desc'
+          }
+        }
+      }
+    });
+
+    if(!userWithMessages) {
+      return Response.json({
+        success: false,
+        message: "User not found"
+      }, {status: 400})
+    }
+
+    return Response.json({
+      success: true,
+      messages: userWithMessages
+    }, {status: 200})
+  } 
+  catch (error) {
+    console.error("Error while gettig messages", error);
+    return Response.json({
+      success: false,
+      message: "Error while getting messages"
+    }, {status: 500})
+  }
+}
