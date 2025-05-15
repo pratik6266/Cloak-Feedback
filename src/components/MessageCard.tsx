ec2b3cbbbda3f90457cd8981ec6d206c45215226
Pratik Raj <pratikraj220011@gmail.com>
2025-05-15 15:48:54 +0530

    created payload to send data to api with type

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -4,7 +4,6 @@ import React from 'react'
 import {
   Card,
   CardContent,
-  CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
@@ -25,50 +24,65 @@ import { Message } from '@prisma/client'
 import axios from 'axios'
 import { ApiResponse } from '@/types/apiResponse'
 import { toast } from 'sonner'
+import dayjs from 'dayjs';
+
 
 type MessageCardProps = {
   message: Message,
   onMessageDelete: (messageId: string) => void,
 }
 
+interface payloadInterface {
+  id: string,
+}
+
 const MessageCard = ({ message, onMessageDelete }: MessageCardProps) => {
 
+  const payload: payloadInterface = {
+    id: message.id
+  }
+
   const handleDeleteConfirm = async () => {
-    const response = await axios.delete<ApiResponse>(`/api/delete-message`, {
-      params: { id: message.id }
-    });
+    const response = await axios.delete<ApiResponse>(`/api/delete-message`, { data: payload });
     toast(response.data.message);
     onMessageDelete(message.id);
   }
 
   return (
-    <Card>
+    <Card className="card-bordered">
       <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-
-        <AlertDialog>
-          <AlertDialogTrigger asChild>
-            <Button variant="destructive"><X className='w-5 h-5'/></Button>
-          </AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction onClick={handleDeleteConfirm}>Continue</AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-
-        <CardDescription>Card Description</CardDescription>
+        <div className="flex justify-between items-center">
+          <CardTitle>{message.content}</CardTitle>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant='destructive'>
+                <X className="w-5 h-5" />
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  this message.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>
+                  Cancel
+                </AlertDialogCancel>
+                <AlertDialogAction onClick={handleDeleteConfirm}>
+                  Continue
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </div>
+        <div className="text-sm">
+          {dayjs(message.createdAt).format('MMM D, YYYY h:mm A')}
+        </div>
       </CardHeader>
-      <CardContent>
-      </CardContent>
+      <CardContent></CardContent>
     </Card>
   )
 }
