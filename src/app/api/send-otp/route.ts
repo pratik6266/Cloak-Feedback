514ae5df195193ab88c2ce7a95a5f2d900b5b747
Pratik Raj <pratikraj220011@gmail.com>
2025-05-15 18:44:50 +0530

    created otp resend api

---
+++ src/app/api/send-otp/route.ts
@@ -0,0 +1,53 @@
+import prisma from "@/config/db.config";
+import { sendVerificationEmail } from "@/helpers/sendEmailVerification";
+
+export async function POST(req: Request){
+  const { userName }: { userName: string } = await req.json();
+  
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        userName,
+      }
+    })
+  
+    if(!user){
+      return Response.json({
+        success: false,
+        message: "User not found",
+      }, {status: 404});
+    }
+  
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+    const newCodeExpiry: Date = new Date(Date.now() + 3600000);
+    const response = await sendVerificationEmail(user.email, user.userName, verifyCode);
+    if(!response.success){
+      return Response.json({
+        success: false,
+        message: response.message
+      }, {status: 500})
+    }
+
+    await prisma.user.updateMany({
+      where: {
+        userName,
+      },
+      data:{
+        verifyCode,
+        verifyCodeExpiry: newCodeExpiry
+      }
+    })
+  
+    return Response.json({
+      success: true,
+      message: "OTP resend successfull",
+    }, {status: 200})
+  } 
+  catch (error) {
+    console.error("Error while sending opt again", error)
+    return Response.json({
+      success: false,
+      message: "Something went wrong while otp resend"
+    }, {status: 500})
+  }
+}
