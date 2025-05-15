4f0f436de8bb68758b5c3181cb576f15e9cb4775
Pratik Raj <pratikraj220011@gmail.com>
2025-05-15 13:20:07 +0530

    wrapped in tryCatch

--- src/app/api/delete-message/route.ts
+++ src/app/api/delete-message/route.ts
@@ -3,25 +3,33 @@ import prisma from "@/config/db.config";
 export async function DELETE (req: Request){
   const { id } = await req.json();
   
-  const message =  await prisma.message.findUnique({
-    where:{
-      id,
+  try {
+    const message =  await prisma.message.findUnique({
+      where:{
+        id,
+      }
+    })
+    if(!message){
+      return Response.json({
+        success: false,
+        message: "Message don't exist"
+      }, { status: 400 });
     }
-  })
-  if(!message){
+  
+    await prisma.message.delete({
+      where:{
+        id,
+      }
+    })
+    return Response.json({
+      success: true,
+      message: "Deleted Successfully"
+    }, {status: 200})
+  } catch (error) {
+    console.error("Error while deleting message", error);
     return Response.json({
       success: false,
-      message: "Message don't exist"
-    }, { status: 400 });
+      message: "Error while deleting message"
+    }, { status: 500 });
   }
-
-  await prisma.message.delete({
-    where:{
-      id,
-    }
-  })
-  return Response.json({
-    success: true,
-    message: "Deleted Successfully"
-  }, {status: 200})
 }
