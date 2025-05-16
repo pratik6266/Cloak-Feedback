38eda853b5930813e0a68b36252ac854896b66e2
Pratik Raj <pratikraj220011@gmail.com>
2025-05-16 19:04:37 +0530

    updated css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,8 +14,8 @@ const Navbar = () => {
   return (
     <>  
       <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <a href="/dashboard" className="text-xl font-bold mb-4 md:mb-0 text-gray-300 hover:text-gray-100 hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer">
+      <div className="container flex justify-between items-center">
+        <a href="/dashboard" className="sm: text-xl font-bold mb-4 md:mb-0 text-gray-300 hover:text-gray-100 hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer">
           Cloak Feedback
         </a>
         {session ? (
@@ -25,13 +25,13 @@ const Navbar = () => {
                 Welcome, {user.userName || user.email}
               </span>
             </Link>
-            <Button onClick={() => signOut()} className="w-full md:w-auto bg-slate-100 text-black px-6 py-2 rounded-xl shadow-md transition-all duration-300 ease-in-out hover:scale-100 hover:text-base hover:bg-red-600 hover:text-white" variant='outline'>
+            <Button onClick={() => signOut()} className=" bg-slate-100 text-black px-6 py-2 rounded-xl shadow-md transition-all duration-300 ease-in-out hover:scale-100 hover:text-base hover:bg-red-600 hover:text-white" variant='outline'>
               Logout
             </Button>
           </>
         ) : (
           <Link href="/sign-in">
-            <Button className="w-full md:w-auto bg-slate-100 text-black px-6 py-2 rounded-xl shadow-md transition-all duration-300 ease-in-out hover:scale-100 hover:text-base hover:bg-gray-700 hover:text-white" variant={'outline'}>Login</Button>
+            <Button className=" bg-slate-100 text-black px-6 py-2 rounded-xl shadow-md transition-all duration-300 ease-in-out hover:scale-100 hover:text-base hover:bg-gray-700 hover:text-white" variant={'outline'}>Login</Button>
           </Link>
         )}
       </div>
