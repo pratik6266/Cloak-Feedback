e48a1c89d1f016361199a9e056b2a5212d4aba7e
Pratik Raj <pratikraj220011@gmail.com>
2025-05-16 19:43:00 +0530

    navbar css bug fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,13 +25,13 @@ const Navbar = () => {
                 Welcome, {user.userName || user.email}
               </span>
             </Link>
-            <Button onClick={() => signOut()} className=" bg-slate-100 text-black px-6 py-2 rounded-xl shadow-md transition-all duration-300 ease-in-out hover:scale-100 hover:text-base hover:bg-red-600 hover:text-white" variant='outline'>
+            <Button onClick={() => signOut()} className="bg-slate-100 text-black px-6 py-2 rounded-xl shadow-md transition-all duration-300 ease-in-out hover:scale-100 hover:text-base hover:bg-red-600 hover:text-white" variant='outline'>
               Logout
             </Button>
           </>
         ) : (
           <Link href="/sign-in">
-            <Button className=" bg-slate-100 text-black px-6 py-2 rounded-xl shadow-md transition-all duration-300 ease-in-out hover:scale-100 hover:text-base hover:bg-gray-700 hover:text-white" variant={'outline'}>Login</Button>
+            <Button className="bg-slate-100 text-black px-6 py-2 rounded-xl shadow-md transition-all duration-300 ease-in-out hover:scale-100 hover:text-base hover:bg-gray-700 hover:text-white" variant={'outline'}>Login</Button>
           </Link>
         )}
       </div>
