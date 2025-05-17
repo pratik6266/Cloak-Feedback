f0a039b9065daef4511c6066bf81ff86cb3621b8
Pratik Raj <pratikraj220011@gmail.com>
2025-05-17 13:03:55 +0530

    eslint fix for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: false,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // reactStrictMode: false,
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
   eslint:{
     ignoreDuringBuilds: true,
   },
